bf8d3548fbeb207c4a2e2b4a32bd206e57dd1fc8
ssandyy <ssandyy789@gmail.com>
2024-09-24 15:20:31 +0530

    guess game complete

--- dom-project-BasicJs-main/4-GuessTheNumber/chaiaurcode.js
+++ dom-project-BasicJs-main/4-GuessTheNumber/chaiaurcode.js
@@ -1,19 +1,19 @@
 
 const randomNum = (parseInt(Math.random() * 10) + 1);
 
-console.log(randomNum);
+//console.log(randomNum);
 
 const submit = document.querySelector("#subt")
 const guessField = document.querySelector('#guessField');
 const result = document.querySelector(".lastResult")
 const gussesDone = document.querySelector(".guesses");
-const resultArea = document.querySelector('.resultParas')
+const remainingTry = document.querySelector('.remainingTry')
 const lowOrHigusses = document.querySelector('.lowOrHi')
+const resultArea = document.querySelector(".resultParas")
 
 
 let previousGuess = []
 let start = true
-let defaultGuess = 1
 let maxTry = 5
 
 if (start) {
@@ -26,11 +26,13 @@ if (start) {
 
 function validate(guess) {
     if (isNaN(guess)) {
-        alert(`Please enter valid number to win between 1 to 10`)
+        alert(`Please enter valid lucky number between 1 to 10 to win the jackpot..!`)
     } else if (guess > 10) {
-        alert(`please enter lesser number...!`);
+        // alert(`please enter lesser number...!`);
+        lowOrHigusses.textContent = `please enter lower number..!`;
     } else if (guess < 1) {
-        alert(`please enter higher number..!`);
+        // alert(`please enter higher number..!`);
+        lowOrHigusses.textContent = `please enter higher number..!`;
     } else {
         checkGuess(guess)
     }
@@ -38,25 +40,29 @@ function validate(guess) {
 
 function checkGuess(guess) {
     if (guess === randomNum) {
+        guessDisplay(guess)
         result.textContent = `Congratulation you win the game..!`;
         endGame();
     } else {
-        console.log(`sorry this is not the lucky number..!`);
         guessDisplay(guess)
+        result.textContent = `sorry this is not the lucky number..!`
+        
     }
 }
 
-function messageDisplay() { }
-
-
-function guessDisplay(guess) { 
-    if(previousGuess.length < maxTry){
-        previousGuess.push(guess)
-        gussesDone.textContent = previousGuess.join(', ');
-        messageDisplay(guess);
-    }else{
-        result.textContent = `Sorry Game over, The Lucky number was ${randomNum}.`;
+function guessDisplay(guess) {
+    if (previousGuess.length >= maxTry) {
+        result.textContent = `Sorry, Game over! The Lucky number was ${randomNum}`;
         endGame();
+    } else {
+        previousGuess.push(guess);
+        gussesDone.textContent = previousGuess.join(', ');
+        remainingTry.textContent = `${maxTry - previousGuess.length}`;
+        
+        if (previousGuess.length === maxTry) {
+            result.textContent = `Sorry, Game over! The Lucky number was ${randomNum}`;
+            endGame();
+        }
     }
 }
 
