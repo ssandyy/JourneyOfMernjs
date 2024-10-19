c8af1a3b81f7f4bc9342a6d7a6876c7a484871e0
ssandyy <42768759+ssandyy@users.noreply.github.com>
2024-10-19 21:50:50 +0000

    finalising app for debugging

--- JourneyOfReact/CurrencyConvertor/src/components/InputBox.jsx
+++ JourneyOfReact/CurrencyConvertor/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useId } from "react";
 
 function InputBox({
   label,
@@ -11,11 +11,18 @@ function InputBox({
   currencyDisabled=false,
   className = "",
 }) {
+
+  const amountInputId = useId(); //this will generate random unique value
+
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label
+        htmlFor={amountInputId}
+        className="text-black/40 mb-2 inline-block">{label}</label>
+
         <input
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
@@ -26,13 +33,15 @@ function InputBox({
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
-        <select value={selectCurrency} 
+
+        <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+        value={selectCurrency} 
         onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)} 
-        disabled={currencyDisabled} 
-        className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none">
+        disabled={currencyDisabled}
+        >
           
           {currencyOptions.map((currency) => (
-            <option value={currency}>{currency}</option>
+            <option key={currency} value={currency}>{currency}</option>
           ))}
           
         </select>
