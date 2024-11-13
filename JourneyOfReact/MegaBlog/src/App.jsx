a0c18d07e211e0cc651dd74ada2529a9d5964167
ssandyy <ssandyy789@gmail.com>
2024-11-13 15:55:00 +0530

    MegaBlog - React-Redux-Appwrite

---
+++ JourneyOfReact/MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import config from './config/config';
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  console.log(appwriteUrl);
+  
+
+  return (
+    <>
+      <h2>Mega Blog with AppWrite..!</h2>
+    </>
+  )
+}
+
+export default App
