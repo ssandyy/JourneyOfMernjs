44995395e2c9a9c92570a960707e893de81a99e3
ssandyy <ssandyy789@gmail.com>
2025-04-10 08:15:49 +0530

    Promises

--- Javascript2025Revison/AdvanceProject/promises.js
+++ Javascript2025Revison/AdvanceProject/promises.js
@@ -17,7 +17,7 @@ promiseOne.then(function(){
     console.log("Promise one consumed"); // this will invoke by 'resolve()'
 })
 
-// Or method-2 
+// Or method-2  nameless promise
 new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("asyn promise-2 ");
@@ -26,3 +26,60 @@ new Promise(function(resolve, reject){
 }).then(function(){
     console.log("async promise-2 resolved..!");
 })
+
+
+//=====================================================================
+
+const promiseTwo = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({userName: "Sandeep", age: 30}) // use can use to manupulate data from database
+    }, 3000)
+})
+promiseTwo.then(function(userData){
+    console.log(userData);
+    console.log(userData.userName);
+    console.log(userData.age);
+})
+
+//========== use of 'reject' and catch, finally   ====================
+
+const promiseThree = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        let error = true
+        if(!error){
+            resolve({userName: "Sandeep", age: 30, city: "Noida"})
+        }else{
+            reject('error: Something went wrong, please check again..!')
+        }
+
+    }, 1000)
+})
+promiseThree.then((userData2) => {
+    console.log(userData2); // if error is true -> error: Something went wrong, please check again..!
+}).catch((error)=>console.log(error))
+.finally(()=> console.log("something happend either resolve or rejected..!"))
+
+
+//===========================  call of resolve using async -await instead of '.then()'  ==================================================
+
+const promiseFour = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        let error = true
+        if(!error){
+            resolve({userName: "Sandeep", age: 30, city: "Noida", state:"Uttar Pradesh"})
+        }else{
+            reject('error: Something went wrong..!')
+        }
+    }, 4000)
+})
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFour
+        console.log(response);
+    }catch(error){
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+// =========================================================================================================
